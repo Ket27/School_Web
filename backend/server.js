bef019718779a07f01ae3fdb0ed1284eeadf7b2e
Ketan Mattoo <112501232+Ket27@users.noreply.github.com>
2023-07-04 15:55:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,30 @@
 const express = require("express")
+const bodyParser = require('body-parser');
 const dotenv = require ("dotenv");
 const connectDB = require("./config/db")
 const app = express();
-const userRouter = require("./Routes/userRouter")
+const userRouter = require("./Routes/userRouter");
+const attendRouter = require("./Routes/attendRouter");
+const timetableRouter=require("./Routes/timetable")
 app.use(express.json());
+app.use(bodyParser.json());
+
+const path = __dirname + '/views/';
+app.use(express.static(path));
+
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.get('/', (req, res) => {
-    res.send("App is listening");
+    res.sendFile(path + "index.html")
+    // res.send("App is listening");
 })
 
 app.use("/api/user", userRouter)
+app.use("/api/timetable",timetableRouter)
+app.use("/api/attendance", attendRouter)
 
 app.get('/api/currentUser', (req, res) => {
     if (req.session && req.session.user) {
