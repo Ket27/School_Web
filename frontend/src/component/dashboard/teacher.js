e2a50aa315de32aef1e7f0c0e6ac2b60c538471c
Ketan Mattoo <112501232+Ket27@users.noreply.github.com>
2023-07-04 15:42:12 +0530

    Update teacher.js

--- frontend/src/component/dashboard/teacher.js
+++ frontend/src/component/dashboard/teacher.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
+import { HStack, Box, Flex, Button, Text } from '@chakra-ui/react';
+import Sidebar from '../Miscellenous/SideBar';
+
 
 const Teacher = () => {
+  const location = useLocation();
+  // const myProps = location.state;
+
+  // console.log(myProps.prop1);
   return (
-    <div>
-      Hello Teacher
-    </div>
+    <Sidebar/>
+
   )
 }
 
