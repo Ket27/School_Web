da8e0e7efd8a1819375db2767aa3628e0e9e7c70
Ketan Mattoo <112501232+Ket27@users.noreply.github.com>
2023-07-04 15:49:31 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,27 @@
+
 import './App.css';
 import {Route} from "react-router-dom";
 import landingPage from "./pages/LandingPage";
 import teacher from "./component/dashboard/teacher"
 import student from "./component/dashboard/Student"
+import attendance from './component/attendance/attendance';
+import StudentList from './component/attendance/StudentList'
+import studentAttendance from './component/attendance/studentAttendance';
+import timetable from './component/timetable'
+import teacher_timetable from './component/teacher_timetable'
+
 
 function App() {
   return (
     <div className="App">
       <Route path = "/" component = {landingPage} exact/>
-      <switch>
-        <Route path = "/teacher" component = {teacher} exact/>
-        <Route path = "/student" component = {student} exact/>
-      </switch>
+      <Route path = "/teacher" component = {teacher} exact/>
+      <Route path = "/student" component = {student} exact/>
+      <Route path = "/teacher/attendance" component = {attendance} exact/>
+      <Route path = "/student/attendance" component = {studentAttendance} exact/>
+      <Route path = "/teacher/attendance/StudentList/:Class" component = {StudentList} exact/>
+      <Route path = "/student/timetable" component = {timetable} exact/>
+      <Route path = "/teacher/timetable" component = {teacher_timetable} exact/>
   </div>
   );
 }
