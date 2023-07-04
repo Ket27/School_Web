87b47539613ccb30da1db52805734c6eddb66832
Ketan Mattoo <112501232+Ket27@users.noreply.github.com>
2023-07-04 15:50:42 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,11 +8,11 @@ import { ChakraProvider } from '@chakra-ui/react'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <ChakraProvider>
       <App />
     </ChakraProvider> 
-    </BrowserRouter> 
+    </HashRouter> 
   </React.StrictMode>
 );
 
